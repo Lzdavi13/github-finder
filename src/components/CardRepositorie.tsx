aae37803a5c770cd283b6db6bea6855f9b90cc34
Luiz D O Santos <lzdavi1325@gmail.com>
2024-01-04 20:05:29 -0300

    informações do usuario adicioonadas

--- src/components/CardRepositorie.tsx
+++ src/components/CardRepositorie.tsx
@@ -1,4 +1,10 @@
-export function CardRepositorie() {
+import { RepositorieProps } from "../types/repositorie";
+
+type CardRepositorieProps = {
+  repositorieData: RepositorieProps;
+};
+
+export function CardRepositorie({ repositorieData }: CardRepositorieProps) {
   return (
     <div className="flex flex-col gap-4 w-96 border-[1px] border-slate-500 h-32 p-4 rounded-md">
       <div className="flex items-center gap-3">
@@ -15,16 +21,18 @@ export function CardRepositorie() {
           <path d="M13 21V23.5L10 21.5L7 23.5V21H6.5C4.567 21 3 19.433 3 17.5V5C3 3.34315 4.34315 2 6 2H20C20.5523 2 21 2.44772 21 3V20C21 20.5523 20.5523 21 20 21H13ZM13 19H19V16H6.5C5.67157 16 5 16.6716 5 17.5C5 18.3284 5.67157 19 6.5 19H7V17H13V19ZM19 14V4H6V14.0354C6.1633 14.0121 6.33024 14 6.5 14H19ZM7 5H9V7H7V5ZM7 8H9V10H7V8ZM7 11H9V13H7V11Z"></path>
         </svg>
 
-        <p className="text-blue-700 text-lg">academy-templates</p>
+        <p className="text-blue-700 text-lg  hover:underline">
+          <a className="no-underline" href={repositorieData.html_url}>
+            {repositorieData.name}
+          </a>
+        </p>
 
         <span className="text-slate-500 text-sm text-center border-[1px] border-slate-500 px-2 h-6 rounded-xl">
           public
         </span>
       </div>
       <div>
-        <p className="text-slate-500 text-sm">
-          Aqui estão alguns exercicios para praticar
-        </p>
+        <p className="text-slate-500 text-sm">{repositorieData.description}</p>
       </div>
       <div className="flex items-center gap-6">
         <p className="flex text-sm items-center gap-1 text-slate-500">
@@ -39,7 +47,7 @@ export function CardRepositorie() {
           >
             <path d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z"></path>
           </svg>
-          0
+          {repositorieData.stargazers_count}
         </p>
 
         <p className="flex text-sm items-center gap-1 text-slate-500">
@@ -54,7 +62,7 @@ export function CardRepositorie() {
           >
             <path d="M80 104a24 24 0 1 0 0-48 24 24 0 1 0 0 48zm80-24c0 32.8-19.7 61-48 73.3V192c0 17.7 14.3 32 32 32H304c17.7 0 32-14.3 32-32V153.3C307.7 141 288 112.8 288 80c0-44.2 35.8-80 80-80s80 35.8 80 80c0 32.8-19.7 61-48 73.3V192c0 53-43 96-96 96H256v70.7c28.3 12.3 48 40.5 48 73.3c0 44.2-35.8 80-80 80s-80-35.8-80-80c0-32.8 19.7-61 48-73.3V288H144c-53 0-96-43-96-96V153.3C19.7 141 0 112.8 0 80C0 35.8 35.8 0 80 0s80 35.8 80 80zm208 24a24 24 0 1 0 0-48 24 24 0 1 0 0 48zM248 432a24 24 0 1 0 -48 0 24 24 0 1 0 48 0z"></path>
           </svg>
-          1
+          {repositorieData.forks}
         </p>
       </div>
     </div>
--- src/components/Repositories.tsx
+++ src/components/Repositories.tsx
@@ -1,15 +1,48 @@
+import axios, { AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
+import { useUser } from "../hooks/useUser";
 import { CardRepositorie } from "./CardRepositorie";
+import { Loader } from "./Loader";
 
 export function Repositories() {
+  const { user } = useUser();
+  const [repositories, setRepositories] = useState<AxiosResponse[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  async function getRepositories() {
+    try {
+      setIsLoading(true);
+      const response = await axios(
+        `https://api.github.com/users/${user?.login}/repos`
+      );
+
+      const arrayRepositorie = response.data.slice(0, 6);
+
+      setRepositories(arrayRepositorie);
+      setError(false);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getRepositories();
+  }, []);
+
   return (
     <>
       <div className="flex justify-center items-center flex-wrap w-2/4 gap-10 px-6 py-4">
-        <CardRepositorie />
-        <CardRepositorie />
-        <CardRepositorie />
-        <CardRepositorie />
-        <CardRepositorie />
-        <CardRepositorie />
+        {isLoading && <Loader />}
+        {repositories.map((repositorie, indice) => {
+          return (
+            <div key={indice}>
+              <CardRepositorie repositorieData={repositorie} />
+            </div>
+          );
+        })}
       </div>
     </>
   );
