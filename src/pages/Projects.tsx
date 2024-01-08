309072e72b2a56e1db00f5949ff0909687b30103
Luiz D O Santos <lzdavi1325@gmail.com>
2024-01-08 11:11:37 -0300

    ultimos ajustes

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+import arrow from "../assets/arrow-left.svg";
 import { Header } from "../components/Header";
 import { Repositories } from "../components/Repositories";
 
@@ -5,7 +7,15 @@ export function Projects() {
   return (
     <>
       <div className="w-full h-screen bg-zinc-900 flex flex-col gap-16 items-center">
-        <Header />
+        <Header>
+          <Link to={"/"}>
+            <img
+              className="fixed top-9 left-12 w-12 h-12 fill-blue-700 cursor-pointer"
+              src={arrow}
+              alt=""
+            />
+          </Link>
+        </Header>
         <Repositories />
       </div>
     </>
